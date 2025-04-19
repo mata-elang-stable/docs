a69bf71bfd6dd9996f5ec0e522f068ceaa8e5cf1
krisna wahyu setyawan <50494089+ex-dec@users.noreply.github.com>
2025-04-19 07:47:33 +0700

    feat(dashboard): disable blog and modify dashboard

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -95,21 +95,21 @@ const config: Config = {
             }
           },
         },
-        blog: {
-          showReadingTime: true,
-          feedOptions: {
-            type: ["rss", "atom"],
-            xslt: true,
-          },
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          // editUrl:
-          //   "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
-          // Useful options to enforce blogging best practices
-          onInlineTags: "warn",
-          onInlineAuthors: "warn",
-          onUntruncatedBlogPosts: "warn",
-        },
+        // blog: {
+        //   showReadingTime: true,
+        //   feedOptions: {
+        //     type: ["rss", "atom"],
+        //     xslt: true,
+        //   },
+        //   // Please change this to your repo.
+        //   // Remove this to remove the "edit this page" links.
+        //   // editUrl:
+        //   //   "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
+        //   // Useful options to enforce blogging best practices
+        //   onInlineTags: "warn",
+        //   onInlineAuthors: "warn",
+        //   onUntruncatedBlogPosts: "warn",
+        // },
         theme: {
           customCss: "./src/css/custom.css",
         },
@@ -218,15 +218,19 @@ const config: Config = {
               label: "Discord",
               href: "https://discordapp.com/invite/csrg",
             },
+            {
+              label: "Telegram",
+              href: "https://t.me/+BBML3FQIIEgyMTk1"
+            }
           ],
         },
         {
           title: "More",
           items: [
-            {
-              label: "Blog",
-              to: "/blog",
-            },
+            // {
+            //   label: "Blog",
+            //   to: "/blog",
+            // },
             {
               label: "GitHub",
               href: "https://github.com/mata-elang-stable",
